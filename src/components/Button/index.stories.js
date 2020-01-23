9faeb3197c3bc6ca12fa599f5a1d61b696eeec65
Prasanna Poonacha <ppoonacha@mast.com>
2020-01-23 18:12:22 +0530

    Updates

--- src/components/Button/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from './index';
-
-export default { title: 'Button' };
-
-export const pink = () => <Button color='pink'>Pink</Button>;
-export const bbw = () => <Button color='bbw'>BBW</Button>;
-export const vs = () => <Button color='vs'>VS</Button>;
--- src/components/Label/index.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Label from './index';
-
-export default { title: 'Label' };
-
-export const pink = () => <Label color='pink' >Pink</Label>
-export const bbw = () => <Label color='bbw'>BBW</Label>
-export const vs = () => <Label color='vs' >VS</Label>
